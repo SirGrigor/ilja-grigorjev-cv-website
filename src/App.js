e5a389ad5fabe3f400bf500b910794dba8e8f0d3
grigorjev.ilja@gmail.com <werbatim58803110>
2020-11-21 12:09:32 +0200

    Cv-Remastered

--- src/App.js
+++ src/App.js
@@ -5,43 +5,42 @@ import Tab from "./components/mainBoard/DashboardTab";
 import Card from "./components/mainBoard/CardAbout";
 
 function App() {
-  return (
-      <div className="App">
-        <div id = "pBg"><Particles
-            params={{
-              "particles": {
-                "number": {
-                  "value": 110
-                },
-                "size": {
-                  "value": 3
-                }
-              },
-              "interactivity": {
-                "events": {
-                  "onhover": {
-                    "enable": true,
-                    "mode": "repulse"
-                  }
-                }
-              }
-            }}/>
-        </div>
-        <div className="App-info">
-          <div className="App-header">
-            <p className="Header-content" ><kbd>Grigorjev Ilja Portfolio</kbd></p>
-            <div className="About-me">
-              <Card />
+    return (
+        <div className="App">
+            <div id="pBg">
+                <Particles
+                    params={{
+                        particles: {
+                            number: {
+                                value: 150,
+                                density: {enable: true, value_area: 1000,}
+                            },
+                            move: {enable: true, speed: 6, direction: "none", random: true, bounce: true},
+                            line_linked: {enable: true, distance: 150, color: "#000000", opacity: 0.4, width: 1},
+                            color: "#ff0000"
+                        },
+                        background:{
+                            color: "#11b0c9",
+                        }
+                        }
+
+                    } />
             </div>
-          </div>
-          <div className="App-information">
-            <div className="App-content">
-              <Tab />
+            <div className="App-info">
+                <div className="App-header">
+                    <p className="Header-content"><kbd>Grigorjev Ilja Portfolio</kbd></p>
+                    <div className="About-me">
+                        <Card/>
+                    </div>
+                </div>
+                <div className="App-information">
+                    <div className="App-content">
+                        <Tab/>
+                    </div>
+                </div>
             </div>
-          </div>
         </div>
-      </div>
-  );
+    );
 }
 
 export default App;
